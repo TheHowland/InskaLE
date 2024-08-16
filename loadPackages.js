0bdcbff994b2da4b7e9b5dea7b1bf6a3b5ea98c5
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2024-08-16 08:33:01 +0200

    InskaLEV0.3 patch-2

--- loadPackages.js
+++ loadPackages.js
@@ -2,7 +2,7 @@ async function load_packages(pyodide, optAddNames) {
     createProgressBar("Loading Pyodide packages, please wait ...");
     updateProgressBar(0);
     let packageAddress = serverAddress + "/Packages/";
-    let packages = await fetchDirectoryListing(packageAddress, ".whl");
+    let packages = await fetchGitHubDirectoryContents('TheHowland', 'InskaLE', 'Packages', ".whl");
 
     if(Array.isArray(optAddNames)){
         for(let i = 0; i < optAddNames.length; i++){
