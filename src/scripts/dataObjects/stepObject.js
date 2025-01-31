d267e3a673c93b9665c79522b72f2adb93ac38de
Automated Release Development <yannick.wieland@hs-pforzheim.de>
2025-01-31 15:12:16 +0000

    auto update dev release

---
+++ src/scripts/dataObjects/stepObject.js
@@ -0,0 +1,72 @@
+class StepObject {
+    step = ""
+    canBeSimplified = false
+    simplifiedTo = {}
+    componentsRelation = ""
+    components = []  // Class component
+    allComponents = []  // Class component
+    svgData = ""
+
+    getZVal(component) {
+        return component.hasConversion ? component.Z.val : component.Z.impedance;
+    }
+
+    getComponentTypes() {
+        let r, l, c = false
+        this.components.forEach((component) => {
+            if (component.Z.name.includes("R")) {
+                r = true
+            } else if (component.Z.name.includes("L")) {
+                l = true
+            } else if (component.Z.name.includes("C")) {
+                c = true
+            }
+        })
+        if (r && l && c) {
+            return "RLC"
+        } else if (r && l) {
+            return "RL"
+        } else if (r && c) {
+            return "RC"
+        } else if (l && c) {
+            return "LC"
+        } else if (r) {
+            return "R"
+        } else if (l) {
+            return "L"
+        } else if (c) {
+            return "C"
+        }
+    }
+}
+
+class component {
+    Z = {
+        name: "",
+        impedance: "",
+        val: ""
+    }
+    U = {
+        name: "",
+        val: "",
+    }
+    I = {
+        name: "",
+        val: "",
+    }
+    hasConversion = false
+}
+
+class Step0Object {
+    step = ""
+    source = {}  // Class Source
+    allComponents = []  // Class component
+    componentTypes = ""
+    svgData = ""
+}
+
+class Source {
+    type = ""
+    omega_0 = ""
+    sources = {} // Class components
+}
