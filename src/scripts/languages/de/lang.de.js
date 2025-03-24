26cda14032e400fe7cd3f6adeb5a48df07053df2
Automated Release Development <yannick.wieland@hs-pforzheim.de>
2025-03-24 15:45:54 +0000

    kirchhoff - clean logs

---
+++ src/scripts/languages/de/lang.de.js
@@ -0,0 +1,9 @@
+window.german = {
+    ...window.landingDeTexts,
+    ...window.selectorDeTexts,
+    ...window.alertsDeTexts,
+    ...window.simplifierDeTexts,
+    ...window.kirchhoffDeTexts,
+    ...window.cheatsheetDeTexts,
+    ...window.dataLegalAboutDeTexts,
+}
